45ebd57b51ce20d633fabbed50a5816afd0d6eda
Rutuja-Gorawade <gorawaderutuja@gmail.com>
2019-11-02 15:15:25 +0530

    SignIn_Changes

--- src/app/layouts/signin-layout/signin-layout.component.ts
+++ src/app/layouts/signin-layout/signin-layout.component.ts
@@ -3,8 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-signin-layout',
   template: `
+
     <router-outlet></router-outlet>
   `,
   styles: []
 })
 export class SigninLayoutComponent {}
+
+
