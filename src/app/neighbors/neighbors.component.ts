62199f12bcb6a38887d36624690afd39122598c5
atulpawase <atul.pawase@realizertech.com>
2019-11-28 12:05:10 +0530

    Atul - changes for neighbours

--- src/app/neighbors/neighbors.component.ts
+++ src/app/neighbors/neighbors.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ElementRef } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-neighbors',
@@ -6,46 +7,113 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./neighbors.component.scss']
 })
 export class NeighborsComponent implements OnInit {
-  AdditionalFilterPage=false;
-  additionalfilterflag=false;
+  AdditionalFilterPage = false;
+  additionalfilterflag = false;
   filterGroup = true;
-  showFilter= true;
-  newListGroup= true;
-  newNeighborGroup= true;
-  
-  constructor() { }
+  showFilter = true;
+  newListGroup = true;
+  newNeighborGroup = true;
+  firstFilter: boolean;
+  secondFilter = false;
+  leaseFilter = false;
+  distanceFilter = false;
+  countyFilter = false;
+  operatorFilter = false;
+  playTypeFilter = false;
+  searchFilterModal: ElementRef;
+  newNeighborModal: ElementRef;
+
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
   }
 
-  openAdditionalFilter(){
-    this.AdditionalFilterPage=true;
-    this.additionalfilterflag=true;
+  openAdditionalFilter() {
+    this.AdditionalFilterPage = true;
+    this.additionalfilterflag = true;
   }
- closeAdditionalFilter(){
-   debugger;
-    this.AdditionalFilterPage=false;
-    this.additionalfilterflag=false;
+  closeAdditionalFilter() {
+    debugger;
+    this.AdditionalFilterPage = false;
+    this.additionalfilterflag = false;
 
   }
-  
-  
-  toggleFilterGroup(){
+
+  toggleFilterGroup() {
     this.filterGroup = !this.filterGroup;
   }
 
-  toggleNewListGroup(){
+  toggleNewListGroup() {
     this.newListGroup = !this.newListGroup;
   }
 
-  toggleNewNeighborGroup(){
+  toggleNewNeighborGroup() {
     this.newNeighborGroup = !this.newNeighborGroup;
   }
-  
 
-  showSelectedFilter(){
-this. showFilter= false;
+  searchFilterData() {
+    this.showFilter = false;
+    if (this.filterGroup == true) {
+      this.leaseFilter = true;
+      this.distanceFilter = true;
+      this.countyFilter = false;
+      this.operatorFilter = false;
+      this.playTypeFilter = false;
+    }
+    else {
+      this.leaseFilter = false;
+      this.distanceFilter = false;
+      this.countyFilter = true;
+      this.operatorFilter = true;
+      this.playTypeFilter = true;
+    }
   }
 
-  
+
+  openSearchFilterModal(searchFilterModal) {
+    this.searchFilterModal = searchFilterModal;
+    this.modalService.open(this.searchFilterModal, {
+      backdrop: 'static',
+      backdropClass: 'customBackdrop',
+      size: 'lg'
+    })
+  }
+
+  closeSearchFilter() {
+    this.modalService.dismissAll(this.searchFilterModal)
+  }
+
+  closeLeaseFilter() {
+    this.leaseFilter = false;
+  }
+
+  closeDistanceFilter() {
+    this.distanceFilter = false;
+  }
+
+  closeCountyFilter() {
+    this.countyFilter = false;
+  }
+
+  closeOperatorFilter() {
+    this.operatorFilter = false;
+  }
+
+  closePlayTypeFilter() {
+    this.playTypeFilter = false;
+  }
+
+  openNewNeighborModal(newNeighborModal) {
+    this.newNeighborModal = newNeighborModal;
+    this.modalService.open(this.newNeighborModal, {
+      backdrop: 'static',
+      backdropClass: 'customBackdrop',
+      size: 'lg'
+    })
+  }
+
+  closeNewNeighborModal() {
+    this.modalService.dismissAll(this.newNeighborModal);
+  }
+
 }
