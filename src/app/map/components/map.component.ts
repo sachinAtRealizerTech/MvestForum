a35ead955a24f3b6cbabdba964c804e6790dfb97
pra2win <pra2win@gmail.com>
2020-01-24 17:24:53 +0530

    updated to xlease shapefile and style

--- src/app/map/components/map.component.ts
+++ src/app/map/components/map.component.ts
@@ -1,29 +1,51 @@
-import { Component, OnInit } from '@angular/core';
-import { Map } from 'mapbox-gl';
+import { Component, ChangeDetectorRef,OnInit, OnDestroy } from '@angular/core';
+import { Map, LngLat, MapLayerMouseEvent } from 'mapbox-gl';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent implements OnInit {
-  infoLngLat:any= [-96, 37.8]
-  filterMenu:boolean=false;
-  map:Map;
-  constructor() { }
+export class MapComponent implements OnInit, OnDestroy {
+  selectedPoint:GeoJSON.Feature<GeoJSON.Point> | null;
+  filterMenu: boolean = false;
+  cursorStyle='';
+  map: Map;
+  constructor( private ChangeDetectorRef: ChangeDetectorRef) { }
 
   ngOnInit() {
+
+
   }
-  
+
   onLoad(map: Map) {
-    console.log(map)
     this.map = map;
 
-    const exists = this.map.getLayer('leases');
+    this.map.on("mouseenter", "xleases4-webmer-ceenzs", (e: MapLayerMouseEvent) => {
+    
+      this.cursorStyle='pointer';
+       //this.ChangeDetectorRef.detectChanges();
+     // this.map.getCanvas().style.cursor = 'pointer';
+      
+    });
 
+    this.map.on("mouseleave", "xleases4-webmer-ceenzs", (e: MapLayerMouseEvent) => {
+      this.cursorStyle='';
+    });
+
+    this.map.on("click", "xleases4-webmer-ceenzs", (e) => {
+      this.selectedPoint = null;
+      console.log(e)
+      debugger;
+      this.selectedPoint = (<any>e).features[0];
+      this.ChangeDetectorRef.detectChanges();
+      //console.log(this.selectedPoint.geometry.coordinates[0][0])
+    })
   }
-
-  mapCliked(event){
-    console.log(event)
+  claimLease(){
+    alert(`Thanks for claiming lease!`)
+  }
+  ngOnDestroy() {
+    this.map = null;
   }
 }
